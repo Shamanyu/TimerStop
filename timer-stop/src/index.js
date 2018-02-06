551fe394864b8055159f1c47d4e779f3e6172328
Shubham Shamanyu <shubham.s@GGN000390.local>
2018-02-06 15:00:10 +0530

    Write basic code to display a SPA

--- timer-stop/src/index.js
+++ timer-stop/src/index.js
@@ -1,8 +1,22 @@
+// Import react and redux libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
+// Import Store (Redux)
+import store from './store';
+
+// Import App (React)
 import App from './App';
+
+// Register servier worker
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
